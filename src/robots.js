3defc6a51b383f90c45487d2b6d5442a51cecbf0
Gautam Kumar <gautam.kumar062@gmail.com>
2022-08-08 00:48:15 +0530

    Session 1 implemented

---
+++ src/robots.js
@@ -0,0 +1,61 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Robot 1',
+        username: 'robo1',
+        email: 'robo1@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Robot 2',
+        username: 'robo2',
+        email: 'robo2@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Robot 3',
+        username: 'robo3',
+        email: 'robo3@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Gautam Kumar',
+        username: 'sgk062',
+        email: 'gautam@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Gautam Kumar',
+        username: 'sgk062',
+        email: 'gautam@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Gautam Kumar',
+        username: 'sgk062',
+        email: 'gautam@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Gautam Kumar',
+        username: 'sgk062',
+        email: 'gautam@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Gautam Kumar',
+        username: 'sgk062',
+        email: 'gautam@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Gautam Kumar',
+        username: 'sgk062',
+        email: 'gautam@gmail.com'
+    },{
+        id: 10,
+        name: 'Gautam Kumar',
+        username: 'sgk062',
+        email: 'gautam@gmail.com'
+    }
+]
