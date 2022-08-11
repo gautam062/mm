a5a0d4df3f900a1bf6457daf0f257dd61295b448
Gautam Kumar <gautam.kumar062@gmail.com>
2022-08-11 23:25:16 +0530

    changes

---
+++ src/containers/App.js
@@ -0,0 +1,45 @@
+import React,{Component} from "react";
+import CardList from "../components/CardList";
+import SearchBox from '../components/SearchBox'
+import  '../containers/App.css'
+import Scroll from '../components/Scroll'
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots : [],
+            searchfields: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfields: event.target.value})
+    }
+
+    render(){
+        const {robots, searchfields} = this.state
+        const filteredRobot = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchfields.toLowerCase())
+        })
+        return !robots.length ?
+            <h1 className="tc">Loading...</h1> : 
+            (
+                <div className="tc">
+                    <h1 className="f1">Robofriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filteredRobot} />
+                    </Scroll>
+                </div>
+            );
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=>response.json())
+        .then(users => this.setState({robots : users}));
+    }
+}
+
+export default App;
--- src/robots.js
+++ src/robots.js
@@ -1,61 +1,63 @@
 export const robots = [
     {
         id: 1,
-        name: 'Robot 1',
-        username: 'robo1',
-        email: 'robo1@gmail.com'
+        name: 'Spark',
+        username: 'Spark',
+        email: 'Spark@robo.com'
     },
     {
         id: 2,
-        name: 'Robot 2',
-        username: 'robo2',
-        email: 'robo2@gmail.com'
+        name: 'Mechan',
+        username: 'Mechan',
+        email: 'Mechan@robo.com'
     },
     {
         id: 3,
-        name: 'Robot 3',
-        username: 'robo3',
-        email: 'robo3@gmail.com'
+        name: 'Scyther',
+        username: 'Scyther',
+        email: 'Scyther@robo.com'
     },
     {
         id: 4,
-        name: 'Gautam Kumar',
-        username: 'sgk062',
-        email: 'gautam@gmail.com'
+        name: 'Ranger',
+        username: 'Ranger',
+        email: 'Ranger@robo.com'
     },
     {
         id: 5,
-        name: 'Gautam Kumar',
-        username: 'sgk062',
-        email: 'gautam@gmail.com'
+        name: 'Uquh',
+        username: 'Uquh',
+        email: 'Uquh@robo.com'
     },
     {
         id: 6,
-        name: 'Gautam Kumar',
-        username: 'sgk062',
-        email: 'gautam@gmail.com'
+        name: 'Knave',
+        username: 'Knave',
+        email: 'Knave@robo.com'
     },
     {
         id: 7,
-        name: 'Gautam Kumar',
-        username: 'sgk062',
-        email: 'gautam@gmail.com'
+        name: 'Dustie',
+        username: 'Dustie',
+        email: 'Dustie@robo.com'
     },
     {
         id: 8,
-        name: 'Gautam Kumar',
-        username: 'sgk062',
-        email: 'gautam@gmail.com'
+        name: 'Awuoid',
+        username: 'Awuoid',
+        email: 'Awuoid@robo.com'
     },
     {
         id: 9,
-        name: 'Gautam Kumar',
-        username: 'sgk062',
-        email: 'gautam@gmail.com'
-    },{
-        id: 10,
-        name: 'Gautam Kumar',
-        username: 'sgk062',
-        email: 'gautam@gmail.com'
-    }
+        name: 'Twobit',
+        username: 'Twobit',
+        email: 'Twobit@robo.com'
+    },
+    {
+        id: 4,
+        name: 'Alim',
+        username: 'Alim',
+        email: 'Alim@robo.com'
+    },
+    
 ]
